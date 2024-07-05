08635d92404068d212e7a14f9aa818d14487e542
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-05 19:44:42 +0530

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,9 +1,9 @@
-const test={
-    name : "CARZ : An World full of the cars and the toys",
+const repository = {
+    name : 'CARZ : An World full of the cars and the toys ',
     description : "CARZ: A premier website for toy car enthusiasts, featuring a vast collection of Hot Wheels and other brands, offering easy shopping, detailed product descriptions, and exclusive collector's items." ,
-    year : 2023,
-    role : "front-end developer",
-    avatar : "cars.png",
-    demo : "https://chauhan-yuvraj.github.io/cars/" ,
-    github : "https://github.com/Chauhan-yuvraj/cars" ,
+    year : '2023',
+    role : 'front-end developer',
+    avatar : 'cars.png',
+    demo : 'https://chauhan-yuvraj.github.io/cars/' ,
+    github : 'https://github.com/Chauhan-yuvraj/cars' ,
 }
