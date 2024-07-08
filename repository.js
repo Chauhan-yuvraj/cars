ed50000f788e03ec2e10004591c168cefa2df75d
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-08 17:51:56 +0530

    Update repository.js

--- repository.js
+++ repository.js
@@ -3,7 +3,7 @@
     "description": "CARZ: A premier website for toy car enthusiasts, featuring a vast collection of Hot Wheels and other brands, offering easy shopping, detailed product descriptions, and exclusive collector's items.",
     "year": "2023",
     "role": "front-end developer",
-    "avatar": "cars.png",
+    "avatar": "https://seeklogo.com/images/R/Rust-eze-logo-A15B4B7A7A-seeklogo.com.png",
     "demo": "https://chauhan-yuvraj.github.io/cars/",
     "github": "https://github.com/Chauhan-yuvraj/cars"
 }
